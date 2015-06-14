7a8976fdda66544fdaa0f6027f4392edfb7c673e
Tristan <293542476@qq.com>
2015-06-14 10:45:31 +0800

    Initial Commit

---
+++ Qiuxingka/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Qiuxingka
+//
+//  Created by LiuZhiqi on 15/6/14.
+//  Copyright (c) 2015年 Tristan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Qiuxingka/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Qiuxingka
+//
+//  Created by LiuZhiqi on 15/6/14.
+//  Copyright (c) 2015年 Tristan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
